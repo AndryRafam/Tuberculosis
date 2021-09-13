967e15fb3c080959da5110b58c83916f975bb47b
Andry Rafam <andryrafam@protonmail.com>
2021-09-13 09:22:24 +0300

    Add files via upload

---
+++ VGG19/vgg19.py
@@ -0,0 +1,133 @@
+import tensorflow as tf
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+from zipfile import ZipFile
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+random.seed(42)
+np.random.seed(42)
+tf.random.set_seed(42)
+
+with ZipFile("archive.zip","r") as zip:
+    zip.extractall()
+
+batch_size = 32
+img_size = (224,224)
+
+train_ds = image_dataset_from_directory(
+    "TB_Chest_Radiography_Database",
+    validation_split = 0.2,
+    subset = "training",
+    seed = 42,
+    shuffle = True,
+    image_size = img_size,
+    batch_size = batch_size,
+)
+
+val_ds = image_dataset_from_directory(
+    "TB_Chest_Radiography_Database",
+    validation_split = 0.2,
+    subset = "validation",
+    seed = 42,
+    shuffle = True,
+    image_size = img_size,
+    batch_size = batch_size,
+)
+
+from tensorflow.data.experimental import cardinality
+
+val_batches = cardinality(val_ds)
+test_ds = val_ds.take(val_batches//5)
+val_ds = val_ds.skip(val_batches//5)
+
+class_names = train_ds.class_names
+print(class_names)
+
+plt.figure(figsize=(10,10))
+for images,labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3,3,i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+plt.show()
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers.experimental.preprocessing import RandomZoom
+
+data_augmentation = Sequential([
+    RandomZoom(0.2),
+])
+
+plt.figure(figsize=(10,10))
+for images,_ in train_ds.take(2):
+    for i in range(9):
+        ax = plt.subplot(3,3,i+1)
+        augmented_images = data_augmentation(images)
+        plt.imshow(augmented_images[0].numpy().astype("uint8"))
+        plt.axis("off")
+plt.show()
+
+from tensorflow.keras.applications import vgg19
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras import Input, Model
+
+preprocess_input = vgg19.preprocess_input
+
+IMG_SHAPE = img_size+(3,)
+base_model = VGG19(input_shape=IMG_SHAPE,include_top=False,weights="imagenet")
+image_batch,label_batch = next(iter(train_ds))
+feature_batch = base_model(image_batch)
+
+base_model.trainable = True
+base_model.summary()
+feature_batch_average = GlobalAveragePooling2D()(feature_batch)
+
+fine_tune_at = 11
+
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable = False
+
+class Transfer_Vgg19():
+    def model(self,y):
+        self.x = data_augmentation(y)
+        self.x = preprocess_input(self.x)
+        self.x = base_model(self.x,training=False)
+        self.x = GlobalAveragePooling2D()(self.x)
+        self.x = Flatten()(self.x)
+        self.x = Dense(64,activation="relu")(self.x)
+        self.x = Dropout(0.4,seed=42)(self.x)
+        self.x = Dense(32,activation="relu")(self.x)
+        self.x = Dropout(0.4,seed=42)(self.x)
+        self.outputs = Dense(2,activation="sigmoid")(self.x)
+        self.model = Model(y,self.outputs)
+        return self.model
+
+m = Transfer_Vgg19()
+model = m.model(Input(shape=IMG_SHAPE))
+model.summary()
+model.compile(Adam(learning_rate=1e-5),SparseCategoricalCrossentropy(),metrics=["accuracy"])
+
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
+
+if __name__=="__main__":
+    loss0,accuracy0 = model.evaluate(val_ds)
+    print("Initial loss: {:.2f} %".format(100*loss0))
+    print("Initial accuracy: {:.2f} %".format(100*accuracy0))
+    checkpoint = ModelCheckpoint("tuberculosis.hdf5",save_weights_only=False,monitor="val_loss",save_best_only=True)
+    model.fit(train_ds,epochs=5,validation_data=val_ds,callbacks=[checkpoint])
+    best = load_model("tuberculosis.hdf5")
+    loss,accuracy = best.evaluate(test_ds)
+    print("\nTest accuracy: {:.2f} %".format(100*accuracy))
+    print("Test loss: {:.2f} %".format(100*loss))
