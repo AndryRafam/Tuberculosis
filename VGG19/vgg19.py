5210baca3a9c01375cade5f2cf4c91a3f3fecdb9
Andry Rafam <andryrafam@protonmail.com>
2021-10-02 13:58:03 +0300

    Add files via upload

---
+++ VGG19/vgg19.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+from zipfile import ZipFile
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+random.seed(42)
+np.random.seed(42)
+tf.random.set_seed(42)
+
+def unzip(nm):
+    with ZipFile(nm,"r") as zip:
+        zip.extractall()
+
+unzip("archive.zip")
+
+
+train_ds = image_dataset_from_directory(
+    "TB_Chest_Radiography_Database",
+    validation_split = 0.2,
+    subset = "training",
+    seed = 42,
+    shuffle = True,
+    image_size = (224,224),
+    batch_size = 32,
+)
+
+val_ds = image_dataset_from_directory(
+    "TB_Chest_Radiography_Database",
+    validation_split = 0.2,
+    subset = "validation",
+    seed = 42,
+    shuffle = True,
+    image_size = (224,224),
+    batch_size = 32,
+)
+
+from tensorflow.data.experimental import cardinality
+
+val_batches = cardinality(val_ds)
+test_ds = val_ds.take(val_batches//5)
+val_ds = val_ds.skip(val_batches//5)
+
+class_names = train_ds.class_names
+print(class_names)
+
+plt.figure(figsize=(12,12))
+for images,labels in train_ds.take(1):
+    for i in range(4):
+        ax = plt.subplot(2,2,i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+plt.show()
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
+
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers.experimental.preprocessing import RandomZoom, RandomRotation
+
+data_augmentation = Sequential([
+    RandomZoom(0.2),
+    RandomRotation(0.1),
+])
+
+from tensorflow.keras.applications import vgg19, VGG19
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras import Input, Model
+
+rescale = vgg19.preprocess_input
+
+base_model = VGG19(input_shape=(224,224,3),include_top=False,weights="imagenet")
+base_model.trainable = True
+base_model.summary()
+
+
+fine_tune_at = 11
+
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable = False
+
+class Transfer_VGG19():
+    def model(self,input):
+        self.x = data_augmentation(input)
+        self.x = rescale(self.x)
+        self.x = base_model(self.x,training=False)
+        self.x = GlobalAveragePooling2D()(self.x)
+        self.x = Flatten()(self.x)
+        self.x = Dense(128,activation="relu")(self.x)
+        self.x = Dropout(0.4,seed=42)(self.x)
+        self.x = Dense(64,activation="relu")(self.x)
+        self.x = Dropout(0.4,seed=42)(self.x)
+        self.outputs = Dense(2,activation="sigmoid")(self.x)
+        self.model = Model(input,self.outputs,name="Transfer_VGG19")
+        return self.model
+
+TVGG19 = Transfer_VGG19()
+model = TVGG19.model(Input(shape=(224,224,3)))
+model.summary()
+model.compile(Adam(learning_rate=1e-5),SparseCategoricalCrossentropy(),metrics=["accuracy"])
+
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
+
+if __name__=="__main__":
+    loss0,accuracy0 = model.evaluate(val_ds)
+    print("Initial loss: {:.2f} %".format(100*loss0))
+    print("Initial accuracy: {:.2f} %".format(100*accuracy0))
+    checkpoint = ModelCheckpoint("tuberculosis.hdf5",save_weights_only=False,monitor="val_loss",save_best_only=True)
+    model.fit(train_ds,epochs=5,validation_data=val_ds,callbacks=[checkpoint])
+    best = load_model("tuberculosis.hdf5")
+    val_loss,val_accuracy = best.evaluate(val_ds)
+    test_loss,test_accuracy = best.evaluate(test_ds)
+    print("\nVal accuracy: {:.2f} %".format(100*val_accuracy))
+    print("Val loss: {:.2f} %".format(100*val_loss))
+    print("\nTest accuracy: {:.2f} %".format(100*test_accuracy))
+    print("Test loss: {:.2f} %".format(100*test_loss))
