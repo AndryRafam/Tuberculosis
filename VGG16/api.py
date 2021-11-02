a4c96a9ba329762efbe77daef895ce8484284ee2
Andry Rafam <andryrafam@protonmail.com>
2021-11-02 09:32:55 +0300

    Add files via upload

---
+++ VGG16/api.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+from tensorflow.keras import Sequential
+
+data_augmentation = Sequential([
+    tf.keras.layers.RandomZoom(0.2),
+    tf.keras.layers.RandomRotation(0.1),
+])
+
+rescale = tf.keras.applications.vgg16.preprocess_input
+
+base_model = tf.keras.applications.VGG16(input_shape=(224,224,3),include_top=False,weights="imagenet")
+base_model.trainable = True
+base_model.summary()
+
+for layer in base_model.layers:
+    if layer.name == "block3_pool":
+        break
+    layer.trainable = False
+
+class Transfer_VGG16():
+    def model(self,input):
+        self.x = data_augmentation(input)
+        self.x = rescale(self.x)
+        self.x = base_model(self.x,training=False)
+        self.x = tf.keras.layers.GlobalAveragePooling2D()(self.x)
+        self.x = tf.keras.layers.Flatten()(self.x)
+        self.x = tf.keras.layers.Dense(128,activation="relu")(self.x)
+        self.x = tf.keras.layers.Dropout(0.2,seed=42)(self.x)
+        self.x = tf.keras.layers.Dense(64,activation="relu")(self.x)
+        self.x = tf.keras.layers.Dropout(0.2,seed=42)(self.x)
+        self.output = tf.keras.layers.Dense(2,activation="sigmoid")(self.x)
+        self.model = tf.keras.Model(input,self.output,name="Transfer_VGG16")
+        return self.model
+
+TFVGG16 = Transfer_VGG16()
+model = TFVGG16.model(tf.keras.Input(shape=(224,224,3)))
+model.summary()
+model.compile(tf.keras.optimizers.Adam(1e-5),tf.keras.losses.SparseCategoricalCrossentropy(),metrics=["accuracy"])
--- VGG16/vgg16.py
+++ VGG16/vgg16.py
@@ -60,50 +60,11 @@ val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
 test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
 
 
-from tensorflow.keras import Sequential
-
-data_augmentation = Sequential([
-    tf.keras.layers.RandomZoom(0.2),
-    tf.keras.layers.RandomRotation(0.1),
-])
-
-
-rescale = tf.keras.applications.vgg16.preprocess_input
-
-base_model = tf.keras.applications.VGG16(input_shape=(224,224,3),include_top=False,weights="imagenet")
-base_model.trainable = True
-base_model.summary()
-
-
-for layer in base_model.layers:
-    if layer.name == "block3_pool":
-        break
-    layer.trainable = False
-
-class Transfer_VGG16():
-    def model(self,input):
-        self.x = data_augmentation(input)
-        self.x = rescale(self.x)
-        self.x = base_model(self.x,training=False)
-        self.x = tf.keras.layers.GlobalAveragePooling2D()(self.x)
-        self.x = tf.keras.layers.Flatten()(self.x)
-        self.x = tf.keras.layers.Dense(128,activation="relu")(self.x)
-        self.x = tf.keras.layers.Dropout(0.2,seed=42)(self.x)
-        self.x = tf.keras.layers.Dense(64,activation="relu")(self.x)
-        self.x = tf.keras.layers.Dropout(0.2,seed=42)(self.x)
-        self.output = tf.keras.layers.Dense(2,activation="sigmoid")(self.x)
-        self.model = tf.keras.Model(input,self.output,name="Transfer_VGG16")
-        return self.model
-
-TFVGG16 = Transfer_VGG16()
-model = TFVGG16.model(tf.keras.Input(shape=(224,224,3)))
-model.summary()
-model.compile(tf.keras.optimizers.Adam(1e-5),tf.keras.losses.SparseCategoricalCrossentropy(),metrics=["accuracy"])
-
+import api
 
 if __name__=="__main__":
     checkpoint = tf.keras.callbacks.ModelCheckpoint("tuberculosis.hdf5",save_weights_only=False,save_best_only=True,monitor="val_accuracy")
-    model.fit(train_ds,epochs=5,validation_data=val_ds,callbacks=[checkpoint])
+    api.model.fit(train_ds,epochs=5,validation_data=val_ds,callbacks=[checkpoint])
     best = tf.keras.models.load_model("tuberculosis.hdf5")
     val_loss,val_acc = best.evaluate(val_ds)
     test_loss,test_acc = best.evaluate(test_ds)
