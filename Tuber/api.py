8793da43cb524a7638751ea929f810091eaf8610
Andry Rafam <andryrafam@protonmail.com>
2021-11-02 09:43:16 +0300

    Add files via upload

---
+++ Tuber/api.py
@@ -0,0 +1,39 @@
+from tensorflow.keras.layers import RandomZoom, RandomRotation
+from tensorflow.keras import Sequential
+
+data_augmentation = Sequential([
+	RandomZoom(0.2),
+    RandomRotation(0.1),
+])
+
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Rescaling
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.models import load_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+
+
+class Tuber():
+    def model(self,input):
+        self.x = data_augmentation(input)
+        self.x = Rescaling(1./255)(self.x)
+        self.x = Conv2D(64,3,activation="relu",padding="same",strides=(2,2))(self.x)
+        self.x = MaxPooling2D()(self.x)
+        self.x = Conv2D(128,3,activation="relu",padding="same",strides=(2,2))(self.x)
+        self.x = Conv2D(128,3,activation="relu",padding="same",strides=(2,2))(self.x)
+        self.x = Conv2D(256,3,activation="relu",padding="same",strides=(2,2))(self.x)
+        self.x = MaxPooling2D()(self.x)
+        self.x = Flatten()(self.x)
+        self.x = Dense(128,activation="relu")(self.x)
+        self.x = Dropout(0.2,seed=123)(self.x)
+        self.x = Dense(64,activation="relu")(self.x)
+        self.x = Dropout(0.2,seed=123)(self.x)
+        self.outputs = Dense(2,activation="sigmoid")(self.x)
+        self.model = Model(input,self.outputs,name="Tuber")
+        return self.model
+
+tuber = Tuber()
+model = tuber.model(Input(shape=(224,224,3)))
+model.summary()
+model.compile(Adam(),SparseCategoricalCrossentropy(),metrics=["accuracy"])
--- Tuber/tuber.py
+++ Tuber/tuber.py
@@ -59,52 +59,16 @@ train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
 test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
 
-from tensorflow.keras.layers import RandomZoom, RandomRotation
 
-data_augmentation = tf.keras.Sequential([
-	RandomZoom(0.2),
-    RandomRotation(0.1),
-])
-
-from tensorflow.keras import Input, Model
-from tensorflow.keras.layers import Rescaling
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.models import load_model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
+import api
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.models import load_model
 
-
-class Tuber():
-    def model(self,input):
-        self.x = data_augmentation(input)
-        self.x = Rescaling(1./255)(self.x)
-        self.x = Conv2D(64,3,activation="relu",padding="same",strides=(2,2))(self.x)
-        self.x = MaxPooling2D()(self.x)
-        self.x = Conv2D(128,3,activation="relu",padding="same",strides=(2,2))(self.x)
-        self.x = Conv2D(128,3,activation="relu",padding="same",strides=(2,2))(self.x)
-        self.x = Conv2D(256,3,activation="relu",padding="same",strides=(2,2))(self.x)
-        self.x = MaxPooling2D()(self.x)
-        self.x = Flatten()(self.x)
-        self.x = Dense(128,activation="relu")(self.x)
-        self.x = Dropout(0.2,seed=123)(self.x)
-        self.x = Dense(64,activation="relu")(self.x)
-        self.x = Dropout(0.2,seed=123)(self.x)
-        self.outputs = Dense(2,activation="sigmoid")(self.x)
-        self.model = Model(input,self.outputs,name="Tuber")
-        return self.model
-
-tuber = Tuber()
-model = tuber.model(Input(shape=(224,224,3)))
-model.summary()
-model.compile(Adam(),SparseCategoricalCrossentropy(),metrics=["accuracy"])
-
 if __name__=="__main__":
     initial_epochs = 50
-    loss0,accuracy0 = model.evaluate(val_ds)
+    loss0,accuracy0 = api.model.evaluate(val_ds)
     checkpoint = ModelCheckpoint("tuberculosis.hdf5",save_weights_only=False,monitor="val_accuracy",save_best_only=True)
-    model.fit(train_ds,epochs=initial_epochs,validation_data=val_ds,callbacks=[checkpoint])
+    api.model.fit(train_ds,epochs=initial_epochs,validation_data=val_ds,callbacks=[checkpoint])
     best = load_model("tuberculosis.hdf5")
     val_loss,val_accuracy = best.evaluate(val_ds)
     test_loss,test_accuracy = best.evaluate(test_ds)
