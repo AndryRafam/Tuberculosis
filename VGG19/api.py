ecb8e1fad39bfb6d1716d19ff301482571cacc8e
Andry Rafam <andryrafam@protonmail.com>
2021-11-02 09:29:09 +0300

    Add files via upload

---
+++ VGG19/api.py
@@ -0,0 +1,44 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import RandomZoom, RandomRotation
+
+data_augmentation = Sequential([
+    RandomZoom(0.2),
+    RandomRotation(0.1),
+])
+
+from tensorflow.keras.applications import vgg19, VGG19
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras import Input, Model
+
+rescale = vgg19.preprocess_input
+
+base_model = VGG19(input_shape=(224,224,3),include_top=False,weights="imagenet")
+base_model.trainable = True
+base_model.summary()
+
+fine_tune_at = 11
+
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable = False
+
+class Transfer_VGG19():
+    def model(self,input):
+        self.x = data_augmentation(input)
+        self.x = rescale(self.x)
+        self.x = base_model(self.x,training=False)
+        self.x = GlobalAveragePooling2D()(self.x)
+        self.x = Flatten()(self.x)
+        self.x = Dense(128,activation="relu")(self.x)
+        self.x = Dropout(0.4,seed=42)(self.x)
+        self.x = Dense(64,activation="relu")(self.x)
+        self.x = Dropout(0.4,seed=42)(self.x)
+        self.outputs = Dense(2,activation="sigmoid")(self.x)
+        self.model = Model(input,self.outputs,name="Transfer_VGG19")
+        return self.model
+
+TVGG19 = Transfer_VGG19()
+model = TVGG19.model(Input(shape=(224,224,3)))
+model.summary()
+model.compile(Adam(learning_rate=1e-5),SparseCategoricalCrossentropy(),metrics=["accuracy"])
--- VGG19/vgg19.py
+++ VGG19/vgg19.py
@@ -60,61 +60,17 @@ train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
 test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
 
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import RandomZoom, RandomRotation
 
-data_augmentation = Sequential([
-    RandomZoom(0.2),
-    RandomRotation(0.1),
-])
-
-from tensorflow.keras.applications import vgg19, VGG19
-from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout, Flatten
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.losses import SparseCategoricalCrossentropy
-from tensorflow.keras import Input, Model
-
-rescale = vgg19.preprocess_input
-
-base_model = VGG19(input_shape=(224,224,3),include_top=False,weights="imagenet")
-base_model.trainable = True
-base_model.summary()
-
-
-fine_tune_at = 11
-
-for layer in base_model.layers[:fine_tune_at]:
-    layer.trainable = False
-
-class Transfer_VGG19():
-    def model(self,input):
-        self.x = data_augmentation(input)
-        self.x = rescale(self.x)
-        self.x = base_model(self.x,training=False)
-        self.x = GlobalAveragePooling2D()(self.x)
-        self.x = Flatten()(self.x)
-        self.x = Dense(128,activation="relu")(self.x)
-        self.x = Dropout(0.4,seed=42)(self.x)
-        self.x = Dense(64,activation="relu")(self.x)
-        self.x = Dropout(0.4,seed=42)(self.x)
-        self.outputs = Dense(2,activation="sigmoid")(self.x)
-        self.model = Model(input,self.outputs,name="Transfer_VGG19")
-        return self.model
-
-TVGG19 = Transfer_VGG19()
-model = TVGG19.model(Input(shape=(224,224,3)))
-model.summary()
-model.compile(Adam(learning_rate=1e-5),SparseCategoricalCrossentropy(),metrics=["accuracy"])
-
+import api
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.models import load_model
 
 if __name__=="__main__":
-    loss0,accuracy0 = model.evaluate(val_ds)
+    loss0,accuracy0 = api.model.evaluate(val_ds)
     print("Initial loss: {:.2f} %".format(100*loss0))
     print("Initial accuracy: {:.2f} %".format(100*accuracy0))
     checkpoint = ModelCheckpoint("tuberculosis.hdf5",save_weights_only=False,monitor="val_loss",save_best_only=True)
-    model.fit(train_ds,epochs=5,validation_data=val_ds,callbacks=[checkpoint])
+    api.model.fit(train_ds,epochs=5,validation_data=val_ds,callbacks=[checkpoint])
     best = load_model("tuberculosis.hdf5")
     val_loss,val_accuracy = best.evaluate(val_ds)
     test_loss,test_accuracy = best.evaluate(test_ds)
