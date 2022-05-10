775138915f4bf1ee49bcfd231b820d9adcdc5599
Andry Rafam <andryrafam@protonmail.com>
2022-05-10 14:49:35 +0300

    Add files via upload

---
+++ InceptionV3/api.py
@@ -0,0 +1,35 @@
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import RandomZoom, RandomRotation
+
+data_augmentation = Sequential([
+    RandomZoom(0.2),
+    RandomRotation(0.1),
+])
+
+from tensorflow.keras.applications import inception_v3, InceptionV3
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.keras import Input, Model
+
+rescale = inception_v3.preprocess_input
+
+base_model = InceptionV3(input_shape=(299,299,3),include_top=False,weights="imagenet")
+base_model.trainable = False
+base_model.summary()
+
+
+def model(input):
+    x = data_augmentation(input)
+    x = rescale(x)
+    x = base_model(x,training=False)
+    x = GlobalAveragePooling2D()(x)
+    x = Dropout(0.2,seed=42)(x)
+    outputs = Dense(2)(x)
+    model = Model(input,outputs,name="Transfer_InceptionV3")
+    return model
+
+model = model(Input(shape=(299,299,3)))
+model.summary()
+model.compile(RMSprop(),SparseCategoricalCrossentropy(from_logits=True),metrics=["accuracy"])
+
---
+++ InceptionV3/inception_v3.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+random.seed(42)
+np.random.seed(42)
+tf.random.set_seed(42)
+
+train_ds = image_dataset_from_directory(
+    "TB_Chest_Radiography_Database",
+    validation_split = 0.2,
+    subset = "training",
+    seed = 42,
+    shuffle = True,
+    image_size = (299,299),
+    batch_size = 32,
+)
+
+val_ds = image_dataset_from_directory(
+    "TB_Chest_Radiography_Database",
+    validation_split = 0.2,
+    subset = "validation",
+    seed = 42,
+    shuffle = True,
+    image_size = (299,299),
+    batch_size = 32,
+)
+
+from tensorflow.data.experimental import cardinality
+
+val_batches = cardinality(val_ds)
+test_ds = val_ds.take(val_batches//5)
+val_ds = val_ds.skip(val_batches//5)
+
+class_names = train_ds.class_names
+
+plt.figure(figsize=(12,12))
+for images,labels in train_ds.take(1):
+    for i in range(4):
+        ax = plt.subplot(2,2,i+1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
+plt.show()
+
+AUTOTUNE = tf.data.AUTOTUNE
+train_ds = train_ds.prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.prefetch(buffer_size=AUTOTUNE)
+test_ds = test_ds.prefetch(buffer_size=AUTOTUNE)
+
+
+import api
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.models import load_model
+
+if __name__=="__main__":
+    loss0,accuracy0 = api.model.evaluate(val_ds)
+    print("Initial loss: {:.2f} %".format(100*loss0))
+    print("Initial accuracy: {:.2f} %".format(100*accuracy0))
+    checkpoint = ModelCheckpoint("tuberculosis.hdf5",save_weights_only=False,monitor="val_loss",save_best_only=True)
+    api.model.fit(train_ds,epochs=10,validation_data=val_ds,callbacks=[checkpoint])
+    best = load_model("tuberculosis.hdf5")
+    val_loss,val_accuracy = best.evaluate(val_ds)
+    test_loss,test_accuracy = best.evaluate(test_ds)
+    print("\nVal accuracy: {:.2f} %".format(100*val_accuracy))
+    print("Val loss: {:.2f} %".format(100*val_loss))
+    print("\nTest accuracy: {:.2f} %".format(100*test_accuracy))
+    print("Test loss : {:.2f} %".format(100*test_loss))
